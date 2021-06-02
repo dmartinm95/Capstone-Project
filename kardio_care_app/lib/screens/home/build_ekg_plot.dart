9921f15017848617735c82b8bd165ef0b734d27c
Diego <dmartinm1995@gmail.com>
2021-06-02 09:50:58 -0700

    Removed averaging from build_ekg_plot, now it plots each received data point

--- kardio_care_app/lib/screens/home/build_ekg_plot.dart
+++ kardio_care_app/lib/screens/home/build_ekg_plot.dart
@@ -51,29 +51,29 @@ class _BuildEKGPlotState extends State<BuildEKGPlot> {
       return <LeadData>[LeadData(xIndex, 0)];
     }
 
-    int sum = data.fold(0, (sum, b) => sum + b);
-    int average = (sum / data.length).round();
+    // int sum = data.fold(0, (sum, b) => sum + b);
+    // int average = (sum / data.length).round();
 
-    // for (int i = 0; i < data.length; i++) {
-    try {
-      dataList.add(LeadData(xIndex, average));
-    } catch (e) {
-      print("Error observed while updating DataList: ${e.toString()}");
-    }
+    for (int i = 0; i < data.length; i++) {
+      try {
+        dataList.add(LeadData(xIndex, data[i]));
+      } catch (e) {
+        print("Error observed while updating DataList: ${e.toString()}");
+      }
 
-    if (dataList.length == 100) {
-      dataList.removeAt(0);
-      _chartSeriesController?.updateDataSource(
-        addedDataIndexes: <int>[dataList.length - 1],
-        removedDataIndexes: <int>[0],
-      );
-    } else {
-      _chartSeriesController?.updateDataSource(
-        addedDataIndexes: <int>[dataList.length - 1],
-      );
+      if (dataList.length == 500) {
+        dataList.removeAt(0);
+        _chartSeriesController?.updateDataSource(
+          addedDataIndexes: <int>[dataList.length - 1],
+          removedDataIndexes: <int>[0],
+        );
+      } else {
+        _chartSeriesController?.updateDataSource(
+          addedDataIndexes: <int>[dataList.length - 1],
+        );
+      }
+      xIndex = xIndex + 1;
     }
-    xIndex = xIndex + 1;
-    // }
 
     return dataList;
   }
